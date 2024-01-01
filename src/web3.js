b36e94a74002c938bf26423946e01b05b33acbad
Aris Lacdao <aceraven777@gmail.com>
2024-01-01 17:27:57 +0800

    Fix sepolia slow transaction

--- src/web3.js
+++ src/web3.js
@@ -2,6 +2,11 @@ import Web3 from 'web3';
 
 window.ethereum.request({ method: "eth_requestAccounts" });
 
-const web3 = new Web3(window.ethereum);
+const OPTIONS = {
+    defaultBlock: 'latest',
+    transactionConfirmationBlocks: 1,
+    transactionBlockTimeout: 5,
+};
+const web3 = new Web3(window.ethereum, null, OPTIONS);
 
 export default web3;
